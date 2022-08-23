609325d87a7dc7ac288b8f346e3edf5302d94756
Vitaly <yyyvitdmitr@gmail.com>
2022-08-23 17:51:24 +0300

    Add lesson 04

---
+++ src/main/java/org/geekbrains/TriangleTest.java
@@ -0,0 +1,2 @@
+package org.geekbrains;public class TriangleTest {
+}
--- src/test/java/org/geekbrains/AppTest.java
+++ src/test/java/org/geekbrains/AppTest.java
@@ -1,28 +1,29 @@
 package org.geekbrains;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.geekbrains.TriangleTest.squareTriangle;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest
 {
-
-    public AppTest( String testName )
-    {
-        super( testName );
+    @ParameterizedTest
+    @CsvSource({"3,3,3,3.90","5,4,3,6.00", "7,9,11,31.42"})
+    @DisplayName("Тест площади треугольника.")
+    void TriangleTest(int a, int b, int c, double result) throws MyException {
+        assertEquals(result, squareTriangle(a,b,c));
     }
 
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    public void testApp()
-    {
-        assertTrue( true );
+    @ParameterizedTest
+    @CsvSource({"3,0,3", "5,4,0", "-2,0,5"})
+    @DisplayName("Негативный тест площади треугольника.")
+    void TriangleNegativeTest(int a, int b, int c) throws MyException {
+        Assertions.assertThrows(MyException.class,()->squareTriangle(a,b,c));
     }
 }
